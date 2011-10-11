a0859bae818a8f953032205fde5c496b2ac8c6be
markus.oley@t-online.de <markus.oley@t-online.de@0bca4923-c631-eee9-6ed9-76f2ec8f65ff>
2011-10-11 21:55:04 +0000

    Share project "org.mda.tests" into "https://stage-midi-master.googlecode.com/svn"

---
+++ org.mda.tests/trunk/src/org/mda/tests/AllTestsSuite.java
@@ -0,0 +1,38 @@
+package org.mda.tests;
+
+import maosoft.TransposeServiceTest;
+import maosoft.midiplayer.ChordTest;
+import maosoft.midiplayer.exporter.PDFExporterTest;
+import maosoft.midiplayer.importer.TextImporterTest;
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+import org.junit.runners.Suite.SuiteClasses;
+import org.mda.editor.preview.ui.test.PreviewEditorTest;
+import org.mda.presenter.ui.test.BeamerPresenterTest;
+import org.mda.presenter.ui.test.MidiFileSlideCalculatorTest;
+
+
+@RunWith(Suite.class)
+@SuiteClasses (  {
+ 
+  //Runtime
+  TextImporterTest.class,
+  PDFExporterTest.class,
+  ChordTest.class,
+  TransposeServiceTest.class,
+  
+  
+  
+  //UI
+  PreviewEditorTest.class,
+  
+  BeamerPresenterTest.class, 
+  MidiFileSlideCalculatorTest.class
+  
+  
+  
+}
+)
+public class AllTestsSuite {
+
+}
