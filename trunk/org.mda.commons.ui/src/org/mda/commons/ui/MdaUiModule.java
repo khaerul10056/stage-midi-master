1badd31219807b9da4e2d97067318cf20519e18b
markus.oley@googlemail.com <markus.oley@googlemail.com@0bca4923-c631-eee9-6ed9-76f2ec8f65ff>
2012-02-28 00:19:49 +0000

    Controlling presentation from presentatinperspective

---
+++ trunk/org.mda.commons.ui/src/org/mda/commons/ui/MdaUiModule.java
@@ -0,0 +1,13 @@
+package org.mda.commons.ui;
+
+import com.google.inject.AbstractModule;
+
+
+public class MdaUiModule extends AbstractModule {
+
+  @Override
+  protected void configure () {
+
+  }
+
+}
