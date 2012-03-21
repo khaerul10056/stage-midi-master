1b22d18b4372275ba7d712215e4081a2440fee8b
markus.oley <markus.oley@googlemail.com>
2012-03-21 00:09:28 +0000

    Added google connector tests and integrated useradmin and google connector in application

---
+++ org.mda.tests/src/org/mda/export/TestExportEngine.java
@@ -0,0 +1,17 @@
+package org.mda.export;
+
+import org.junit.Test;
+
+
+public class TestExportEngine {
+
+  @Test
+  public void exportSongbooks () {
+
+    ExportEngine engine = new ExportEngine();
+    engine.exportSongbooks();
+
+
+  }
+
+}
