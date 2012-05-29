42676b04f1be4e4031130ee5d7fc10900fb39a9f
markus.oley@googlemail.com <markus.oley@googlemail.com@0bca4923-c631-eee9-6ed9-76f2ec8f65ff>
2012-05-29 20:39:18 +0000

    some improvements

--- trunk/org.mda.editor.preview.ui/src/org/mda/editor/preview/ui/chords/ChordHover.java
+++ trunk/org.mda.editor.preview.ui/src/org/mda/editor/preview/ui/chords/ChordHover.java
@@ -4,6 +4,7 @@ import org.eclipse.swt.SWT;
 import org.eclipse.swt.custom.StyledText;
 import org.eclipse.swt.events.KeyAdapter;
 import org.eclipse.swt.events.KeyEvent;
+import org.eclipse.swt.graphics.Font;
 import org.eclipse.swt.graphics.Point;
 import org.eclipse.swt.layout.FillLayout;
 import org.eclipse.swt.widgets.Shell;
@@ -11,6 +12,8 @@ import org.eclipse.swt.widgets.Text;
 
 
 public class ChordHover extends Shell {
+
+  private static String history = "";
 
   @Override
   protected void checkSubclass () {
@@ -21,21 +24,35 @@ public class ChordHover extends Shell {
   private final String before;
   private String after;
 
-  public ChordHover (final StyledText styledText, Point point, final String preseletion) {
+  public ChordHover (final Font font, final StyledText styledText, Point point, final String preseletion) {
     super (SWT.NONE);
     this.before = preseletion;
-    setSize(40, 40);
+
+    setSize(120, 40);
     setLocation(point.x,point.y - getSize().y);
     setBackground(getDisplay().getSystemColor(SWT.COLOR_GRAY));
     setLayout(new FillLayout());
     txtChord = new Text(getShell(), SWT.NONE);
+    if (font != null)
+      txtChord.setFont(font);
     txtChord.setText(preseletion);
+    if (preseletion == null || preseletion.trim().length() == 0)  {
+      txtChord.setText(history);
+    }
+
     txtChord.selectAll();
     txtChord.addKeyListener(new KeyAdapter() {
 
       public void keyReleased (final KeyEvent event) {
-        if (event.keyCode == SWT.CR || event.keyCode == SWT.ESC) {
+        if (event.keyCode == SWT.CR) {
           after = txtChord.getText();
+          history = after;
+          dispose();
+        }
+
+        if (event.keyCode == SWT.ESC) {
+          after = preseletion;
+          history = after;
           dispose();
         }
       }
