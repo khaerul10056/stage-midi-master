57a227328f2150b0768ce986e394ceedca36c975
markus.oley <markus.oley@googlemail.com>
2013-02-15 16:19:33 +0000

    ImageCache is used in BeamerPresenter, ControlView

---
+++ org.mda.javafx.presenter/src/org/mda/javafx/presentationcontrol/ActionButtonPane.java
@@ -0,0 +1,62 @@
+package org.mda.javafx.presentationcontrol;
+
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
+import javafx.scene.control.Button;
+import javafx.scene.control.ButtonBuilder;
+import javafx.scene.layout.HBox;
+import javafx.scene.layout.HBoxBuilder;
+import javafx.scene.layout.Pane;
+
+import org.mda.presenter.controller.DefaultPresentationController;
+
+public class ActionButtonPane {
+	
+	public Pane build (final DefaultPresentationController defaultController) {
+		HBox nextPane = HBoxBuilder.create().build();
+		
+		Button btnWhite = createButton("White");
+		btnWhite.setOnAction(new EventHandler<ActionEvent>() {
+			@Override
+			public void handle(ActionEvent arg0) {
+				defaultController.toggleWhite();				
+			}
+		});
+		Button btnBlack = createButton("Black");
+		btnBlack.setOnAction(new EventHandler<ActionEvent>() {
+			@Override
+			public void handle(ActionEvent arg0) {
+				defaultController.toggleBlack();				
+			}
+		});
+		Button btnBackground = createButton("Background");
+		btnBackground.setOnAction(new EventHandler<ActionEvent>() {
+			@Override
+			public void handle(ActionEvent arg0) {
+				defaultController.toggleOnlyBackground();				
+			}
+		});
+		Button btnNormal = createButton("Normal");
+		btnNormal.setOnAction(new EventHandler<ActionEvent>() {
+			@Override
+			public void handle(ActionEvent arg0) {
+				defaultController.toggleNormalize();				
+			}
+		});
+		
+		nextPane.getChildren().addAll(btnWhite, btnBlack, btnBackground, btnNormal);
+		
+		
+		
+		return nextPane;
+	}
+	
+	Button createButton (final String name) {
+		Button btn = ButtonBuilder.create().build();
+		btn.setFocusTraversable(false);
+		btn.setText(name);
+		return btn;
+		
+	}
+
+}
